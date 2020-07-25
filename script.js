3b424d2d7de21b68e8c4e1d50d2973556da14a72
AlDacMac <alasdairmacgsign@gmail.com>
2020-07-25 15:06:19 +0100

    Add scripting for writing a number in the display

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let displayValue = "0";
+let display = document.querySelector("#display");
+let periodIn = false;
+
 const add = (a, b) => {a + b};
 const subtract = (a, b) => {a - b};
 const multiply = (a, b) => {a * b};
@@ -6,3 +10,34 @@ const divide = (a, b) => {a / b};
 const operate = (a, b, operator) => {
     return operator(a, b);
 }
+
+const updateDisplay = () => {
+    display.textContent = displayValue
+}
+
+let numButtons = document.querySelectorAll(".numButton");
+numButtons.forEach((button) => {
+    button.addEventListener('mouseup', () => {
+        if(button.textContent == "."){ // This conditional prevents multiple periods from being entered.
+            if(periodIn){
+                return; 
+            } else {
+                periodIn = true;
+            }
+        }
+        if(displayValue == "0" && button.textContent != "."){
+            displayValue = button.textContent;
+        } else {
+            displayValue += "" + button.textContent;
+        }
+        updateDisplay();
+    });
+});
+
+let clear = document.querySelector("#clear")
+clear.addEventListener('mouseup', () => {
+    displayValue = "0"
+    updateDisplay();
+});
+
+updateDisplay();
